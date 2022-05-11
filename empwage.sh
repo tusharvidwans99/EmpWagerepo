064c13e2d88f804c5f5c2425ad14b7e5242156a2
tusharvidwans99 <tusharvidwans99@gmail.com>
2022-05-11 18:50:53 +0530

    Refracted the code to get work hours from function

--- empwage.sh
+++ empwage.sh
@@ -9,10 +9,9 @@ MAX_DAYS=20
 days=1
 work_completed=0
 MAX_WORK_HOUR_IN_MONTH=100
-while [[ $days -le $MAX_DAYS && $work_completed -lt $MAX_WORK_HOUR_IN_MONTH ]]
-do
-empcheck=$((RANDOM%3))
 
+function work_hours() {
+empcheck=$((RANDOM%3))
 case $empcheck in
 	$isFulltime)
 		daily_work_hr=12
@@ -24,6 +23,14 @@ case $empcheck in
 		daily_work_hr=0
 		;;
 esac
+return $daily_work_hr
+
+}
+
+while [[ $days -le $MAX_DAYS && $work_completed -lt $MAX_WORK_HOUR_IN_MONTH ]]
+do
+empcheck=$((RANDOM%3))
+work_hours
 work_completed=$(( $work_completed+$daily_work_hr ))
 salary=$(( $WAGE_PER_HOUR*$daily_work_hr ))
 echo $salary
