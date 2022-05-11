62ca25952f90c5a673d0035f848c7488ad8a3764
tusharvidwans99 <tusharvidwans99@gmail.com>
2022-05-11 17:13:25 +0530

    Added: Calculating Daily wage

--- empwage.sh
+++ empwage.sh
@@ -1,16 +1,20 @@
 #!/bin/bash -x
 
 echo Welcome to Employee Wage Computation
-WORK_PER_HOUR=20
-isPresent=1
+WAGE_PER_HOUR=20
+isFulltime=1
+isParttime=0
 
 empcheck=$((RANDOM%2))
 
-if [ $empcheck == $isPresent ]
+if [ $empcheck == $isFulltime ]
+then
+	daily_work_hr=12
+elif [ $empcheck == $isParttime ]
 then
 	daily_work_hr=8
 else
 	daily_work_hr=0
 fi
-salary=$(( $WORK_PER_HOUR*$daily_work_hr ))
+salary=$(( $WAGE_PER_HOUR*$daily_work_hr ))
 echo $salary
