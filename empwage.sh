c15c06104929a424c11059641ddd7c400834710b
tusharvidwans99 <tusharvidwans99@gmail.com>
2022-05-11 17:28:05 +0530

    Calculate dailywage using case

--- empwage.sh
+++ empwage.sh
@@ -5,16 +5,19 @@ WAGE_PER_HOUR=20
 isFulltime=1
 isParttime=0
 
-empcheck=$((RANDOM%2))
+empcheck=$((RANDOM%3))
 
-if [ $empcheck == $isFulltime ]
-then
-	daily_work_hr=12
-elif [ $empcheck == $isParttime ]
-then
-	daily_work_hr=8
-else
-	daily_work_hr=0
-fi
+case $empcheck in
+	$isFulltime)
+		daily_work_hr=12
+		;;
+	$isParttime)
+		daily_work_hr=8
+		;;
+	*)
+		daily_work_hr=0
+		;;
+esac
+
 salary=$(( $WAGE_PER_HOUR*$daily_work_hr ))
 echo $salary
