d1c11faf01a6b56c1a7eb26db49c7110fe1928fc
tusharvidwans99 <tusharvidwans99@gmail.com>
2022-05-11 17:07:11 +0530

    Added: Daily Wage

--- empwage.sh
+++ empwage.sh
@@ -1,14 +1,16 @@
 #!/bin/bash -x
 
 echo Welcome to Employee Wage Computation
-
+WORK_PER_HOUR=20
 isPresent=1
 
 empcheck=$((RANDOM%2))
 
 if [ $empcheck == $isPresent ]
 then
-	echo Employee is Present
+	daily_work_hr=8
 else
-	echo Employee is Absent
+	daily_work_hr=0
 fi
+salary=$(( $WORK_PER_HOUR*$daily_work_hr ))
+echo $salary
